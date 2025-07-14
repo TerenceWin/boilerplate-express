2b724056af1ac356b4a17cda33444124e131507f
Terence Win <terencewin@Terences-MacBook-Air.local>
2025-07-14 16:59:18 +0900

    Serve JSON on a Specific Route

--- myApp.js
+++ myApp.js
@@ -7,6 +7,7 @@ app.use('/public', express.static(path.join(__dirname, '/public')));
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'views', 'index.html')); 
+    res.json({message: "Hello json"});
 });
 
 
