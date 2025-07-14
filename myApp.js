1362fb3f47cce32ac43a364c7d2ff11c1010e502
Terence Win <terencewin@Terences-MacBook-Air.local>
2025-07-14 16:30:21 +0900

    Serve Static Assets

--- myApp.js
+++ myApp.js
@@ -1,9 +1,13 @@
 let express = require('express');
+let path = require('path');
+
 let app = express();
-absolutePath = __dirname + '/views/index.html' 
 
 console.log("Hello World");
+app.use(express.static(path.join(__dirname, 'public'))); 
+
 app.get('/', (req, res) => {
+    const absolutePath = path.join(__dirname, 'views', 'index.html');
     res.sendFile(absolutePath); 
 });
 
