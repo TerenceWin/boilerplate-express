d6581b690f2445b79164e968c37b937106f00917
Terence Win <terencewin@Terences-MacBook-Air.local>
2025-07-14 18:21:58 +0900

    Chain MiddleWare to create a Time Server

--- myApp.js
+++ myApp.js
@@ -23,7 +23,14 @@ app.get('/json', (req, res) => {
     res.json({message});
 }); 
 
-
+app.get('/now', (req, res, next) => {
+    req.time = new Date().toString(); 
+    next(); 
+},
+    (req, res) => {
+        res.json({ time: req.time }); 
+}
+);
 
 
 
